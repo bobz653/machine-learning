284e95340642abf1fed1b3b64080f9a4135f2af0
bobz653 <bobz653@sina.com>
2017-06-25 18:00:27 +0800

    softmax and lr in PERL

---
+++ lr_softmax/lr.pl
@@ -0,0 +1,166 @@
+﻿#usr/bin/perl/
+use Clone qw/clone/;
+use Data::Dumper;
+
+use strict ;
+use warnings;
+
+
+my @g_itemarr=();
+
+my $g_item={
+	'data'=>undef,'tag'=>undef
+};
+
+my @g_w=();
+my $step=0.025;
+my $delta=0.00001;
+
+sub XW{
+	my ($data,$w)=@_;
+	my $ret=0;
+	
+	for(my $i=0;$i<@$data;$i++){
+		$ret+=($data->[$i])*($w->[$i]);
+	}
+	return $ret;
+}
+
+
+sub Sigmod{
+	my ($data,$w)=@_;
+	my $val=XW($data,$w);
+	return  1/(1+exp(- 1*$val));
+}
+
+
+sub Target{
+	my ($itemarr,$w)=@_;
+	my $val=0;
+	for(my $i=0;$i<@$itemarr;$i++){
+		 my $data=$itemarr->[$i]->{"data"};
+		 my $tag=$itemarr->[$i]->{"tag"};
+		 my $x=XW($data,$w);
+		 $val+= $x*$tag - log(1+exp($x));
+	}
+	return $val;
+}
+
+sub Gradent{
+	my ($itemarr,$w,$a)=@_;
+	for(my $j=0;$j<@$w;$j++){
+			my $tmp=0;
+			for(my $i=0;$i<@$itemarr;$i++){
+				 my $data=$itemarr->[$i]->{"data"};
+				 my $tag=$itemarr->[$i]->{"tag"};
+				 
+				 $tmp+=($data->[$j])*($tag-Sigmod($data,$w));
+			}
+			$w->[$j]+=$a*$tmp;
+	}
+}
+
+sub predict{
+	my ($data,$tag,$w)=@_;
+	my $val=Sigmod($data,$w);
+	my $predicttag=0;
+	if($val>=0.5){
+		$predicttag=1;
+	}
+	if($predicttag == $tag){
+		return 1;
+	}
+	return 0;
+}
+
+#原始数据
+sub loaddata{
+	my ($infile)=@_;
+	open IN,"<$infile" or die "can't open $infile\n";
+	while(<IN>){
+		chomp();
+		my @tmparr=split /\s+/,$_;
+		my $item=clone $g_item;
+		for(my $j=0;$j<$#tmparr;$j++){
+			push(@{$item->{'data'}},$tmparr[$j])
+		}
+		$item->{'tag'}=$tmparr[$#tmparr];
+		push(@g_itemarr,$item);
+		
+	}
+	close IN;
+}
+
+#显示迭代
+sub display{
+	my ($cnt,$oldv,$newv,$w)=@_;
+	print "iterator time: $cnt  oldv:$oldv  newv:$newv, differnt : ".abs($newv-$oldv)."\n";
+	print "arguments:";
+	foreach (@$w){
+		print $_."\t";
+	}
+	print "\n";
+}
+
+loaddata("train.txt");
+
+
+#N-folder
+my $N=5;
+my $avgprec=0;
+my $partlen=int(($#g_itemarr+1)/$N);
+for(my $i=0;$i<$N;$i++){
+	my @traindata=();
+	my @predictdata=();
+	for(my $j=0;$j<=$#g_itemarr;$j++){
+		if($j>=$i*$partlen && $j<=($i+1)*$partlen){
+			 push(@predictdata,$g_itemarr[$j]);
+		}else{
+			push(@traindata,$g_itemarr[$j]); 
+		}
+		
+	}
+	print "size $#traindata  / $#predictdata \n";
+	#init @w
+	my $item=$g_itemarr[0]->{"data"};
+	@g_w=();
+	foreach(@$item){
+		push(@g_w,0);
+	}
+	my $cnt=1;
+	my $oldval=Target(\@traindata,\@g_w);
+	Gradent(\@traindata,\@g_w,$step);
+	my $newval=Target(\@traindata,\@g_w);
+	#display($cnt,$oldval,$newval,\@g_w);
+	
+	while(abs($newval-$oldval)>$delta){
+		$cnt++;
+		$oldval=$newval;
+		Gradent(\@traindata,\@g_w,$step);
+		$newval=Target(\@traindata,\@g_w);
+		#display($cnt,$oldval,$newval,\@g_w);
+	}
+	display($cnt,$oldval,$newval,\@g_w);
+	print "done !\n";
+	
+	my $total=$#predictdata+1;
+	my $rightnum=0;
+	foreach(@predictdata){
+		if(predict($_->{"data"},$_->{"tag"},\@g_w)==1){
+			 $rightnum++;
+		}
+	}
+	print "precsion $rightnum/$total :".$rightnum/$total."\n";
+	$avgprec+=$rightnum/$total;
+	
+}
+print "argv prec:".$avgprec/$N;
+
+
+
+
+
+#print Dumper $g_itemarr[3];
+
+
+
---
+++ lr_softmax/softmax.pl
@@ -0,0 +1,155 @@
+﻿#usr/bin/perl/
+use Clone qw/clone/;
+use Data::Dumper;
+
+use strict ;
+use warnings;
+
+
+my @g_itemarr=();
+
+my $g_item={
+	'data'=>undef,'tag'=>undef
+};
+
+my @g_w=();
+my @g_h=();#目标值数组
+my $step=0.0025;
+my $delta=0.00001;
+
+sub EXPXW{
+	my ($data,$w)=@_;
+	my $ret=0;
+	
+	for(my $i=0;$i<@$data;$i++){
+		$ret+=($data->[$i])*($w->[$i]);
+	}
+	return exp($ret);
+}
+
+#针对每个样例计算每个类的目标值
+sub Target{
+	my ($itemarr)=@_;
+	#最后一个类的参数是0，所以sum是1
+	my $sum=1;
+	for(my $i=0;$i<@g_h;$i++){
+		$g_h[$i]=EXPXW($itemarr,$g_w[$i]);
+		$sum+=$g_h[$i];
+	}
+	for(my $i=0;$i<@g_h;$i++){
+		$g_h[$i]/=$sum;
+	}
+}
+
+sub Gradent{#随机梯度下降
+	my ($itemarr,$w,$a)=@_;
+	for(my $i=0;$i<@$itemarr;$i++){#所有训练数据，更新theta的值
+		my $data=$itemarr->[$i]->{"data"};
+		my $tag=$itemarr->[$i]->{"tag"};
+		Target($data); #每一个样例，更新一次
+		for(my $j=0;$j<@$w;$j++){#针对不同的类别
+				for(my $n=0;$n<@$data;$n++){
+					$w->[$j]->[$n]+=$a*$data->[$n]*(($j+1==$tag?1:0)-$g_h[$j]);
+				}
+		}
+	}
+}
+
+
+#原始数据
+sub loaddata{
+	my ($infile)=@_;
+	open IN,"<$infile" or die "can't open $infile\n";
+	my %tagnumhsh=();
+	my $thetanum=0;
+	while(<IN>){
+		chomp();
+		my @tmparr=split /\,/,$_;
+		my $item=clone $g_item;
+		$thetanum=$#tmparr;
+		for(my $j=0;$j<$#tmparr;$j++){
+			push(@{$item->{'data'}},$tmparr[$j])
+		}
+		$item->{'tag'}=$tmparr[$#tmparr];
+		$tagnumhsh{$tmparr[$#tmparr]}=1;
+		push(@g_itemarr,$item);
+	}
+	close IN;
+	for(my $i=0;$i<scalar(keys %tagnumhsh) - 1;$i++){
+		my @thetaarr=();
+		for(my $j=0;$j<$thetanum;$j++){
+			push(@thetaarr,0.3);
+		}
+		push(@g_w,\@thetaarr);
+		push(@g_h,0);
+	}
+	
+	print "cat:".scalar(@g_w)."\ttheta dimension $thetanum \n";
+	
+}
+
+
+
+#预测
+sub predict{
+	my ($data)=@_;
+	Target($data->{"data"});	
+	my $lastcat=0;
+	foreach(@g_h){
+		$lastcat+=$_;
+	}
+	print join(",",@g_h).",".(1-$lastcat)."\n";
+}
+
+loaddata("softmaxtrain.txt");
+
+	print "size $#g_itemarr \n";
+  my $cnt=0;
+  my $opnum=100;
+	while(1){
+			my $break=0;
+			$cnt++;
+			if($cnt %100 ==0){
+				print "$cnt ...\n";
+			}
+			if($cnt >1000){
+				#last;
+				}
+			Gradent(\@g_itemarr,\@g_w,$step);
+			
+			#计算当前的目标函数值
+			my $sum=0;
+			foreach(@g_itemarr){
+				my $data=$_->{"data"};
+				my $tag=$_->{"tag"};
+				my $last=1;
+				for(my $i=0;$i<@g_h;$i++){
+					$sum+=($i+1==$tag?1:0)*log($g_h[$i]);
+					$last -=$g_h[$i];
+				}
+				$sum+=($#g_h+1==$tag?1:0)*log($last);
+			}
+			$sum/=scalar(@g_itemarr);
+			$sum*=- 1;
+			
+			my $diff=abs($sum-$opnum);
+			#print sprintf("%.12f\n",$diff);
+			if($diff<$delta){
+				print "iterator number : $cnt \n";
+				last;
+			}
+			
+			
+			$opnum=$sum;
+			
+			
+	}
+	
+	foreach(@g_itemarr){
+		predict($_);
+	}
+
+#print Dumper $g_itemarr[3];
+
+
+
